c81be97a0b31271d51915511f8b8301dd965db8f
xinxin5080 <xinxin5080@163.com>
2018-12-25 11:07:48 +0800

    图标，样式初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './style/iconfont.scss'
+import './style/reset.scss'
 
 Vue.config.productionTip = false
 
